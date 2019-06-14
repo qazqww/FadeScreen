c49d950b254925397ae17a3ab36191fa7e4187c0
ktm-jg <raptarior@gmail.com>
2019-06-14 10:37:42 +0900

    Initial commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int nodeCount = 5;
+    public Node nodePrefab;
+    public Node[,] nodeArr;
+
+    Transform root;
+
+    void Awake()
+    {
+        nodePrefab = Resources.Load<Node>("Node");
+        root = transform.Find("Root");
+        CreateGrid(nodeCount);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            StartCoroutine(Transparent(true));
+        else if (Input.GetKeyDown(KeyCode.T))
+            StartCoroutine(Transparent(false));
+    }
+
+    void CreateGrid(int nodeCount)
+    {
+        this.nodeCount = nodeCount;
+        int count = 0;
+        nodeArr = new Node[nodeCount, nodeCount];
+        
+        for (int row = 0; row < nodeCount; ++row)
+            for (int col = 0; col < nodeCount; ++col)
+            {
+                Node node = Instantiate(nodePrefab, Vector3.zero, Quaternion.identity, root);
+                nodeArr[row, col] = node;
+                node.name = "Node : " + count++;
+                node.SetNode(row, col);
+                node.transform.localPosition = new Vector3(col * 100, -row * 100, 0 );
+            }
+    }
+
+    IEnumerator Transparent(bool b)
+    {
+        for (int row = 0; row < nodeCount; ++row)
+            for (int col = 0; col < nodeCount; ++col)
+            {
+                nodeArr[row, col].SetTransparent(b);
+                yield return new WaitForSeconds(0.1f);
+            }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour
+{
+    bool state;
+    float alpha = 1f;
+    int row;
+    int col;
+    Image image;
+
+    public int Row
+    {
+        get { return row; }
+    }
+    public int Col
+    {
+        get { return col; }
+    }
+
+    public void SetNode(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void SetTransparent(bool b)
+    {
+        StartCoroutine(Transparent(b));
+    }
+
+    public IEnumerator Transparent(bool b)
+    {
+        state = b;
+        if (state)
+        {
+            while (alpha > 0f)
+            {
+                Debug.Log("투명화");
+                alpha -= 0.01f;
+                image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+                yield return null;
+            }            
+        }
+        else
+        {
+            while (alpha < 1f)
+            {
+                Debug.Log("반투명화");
+                alpha += 0.01f;
+                image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+                yield return null;
+            }            
+        }
+        
+    }
+}
